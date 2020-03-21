fc350ff5bf239118c68a889d566295dc9937fb2d
Lahiru Amarathunga <lahiruamarathunge94@gmail.com>
2020-03-21 20:37:34 +0530

    add login

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -10,34 +10,46 @@ import {
 export class LoginScreen extends Component {
   render() {
     return (
-      <SafeAreaView style={{flex: 1}}>
-        <View style={styles.setting}>
-          <Text>Login Screen!</Text>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('HomeApp')}>
-            <Text>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => this.props.navigation.navigate('Register')}>
-            <Text>Register</Text>
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <Text style={styles.subTitle}>Login with email or password</Text>
+        <View style={styles.actions}>
+          <View style={styles.section}></View>
         </View>
-      </SafeAreaView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  setting: {
+  container: {
     flex: 1,
+    backgroundColor: '#FFFFFF',
     justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    //alignItems: 'center',
+  },
+  title: {
+    color: '#3465d9',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: 'gray',
   },
   button: {
     marginTop: 20,
     backgroundColor: 'white',
     padding: 10,
+  },
+  actions: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginTop: 10,
   },
 });
